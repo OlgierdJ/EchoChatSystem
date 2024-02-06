b1aaee87ae698c7ed0721f8756fd99c0c95c45e3
rasmusrbrune <rasmus14.rosenqvist@gmail.com>
2024-02-06 07:46:21 +0100

    added 3 values to enities

--- CoreLib/Entities/EchoCore/SecurityCredentials.cs
+++ CoreLib/Entities/EchoCore/SecurityCredentials.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CoreLib.Entities.Base;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,9 @@ using System.Threading.Tasks;
 
 namespace CoreLib.Entities.EchoCore
 {
-    internal class SecurityCredentials
+    public class SecurityCredentials : BaseEntity<string>
     {
+        public byte[] PasswordHash { get; set; }
+        public byte[] Salt { get; set; }
     }
 }
