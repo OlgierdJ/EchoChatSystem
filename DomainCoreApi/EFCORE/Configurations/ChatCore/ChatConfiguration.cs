5a860cafe2320bc3e0c95750bc1bdc3fb5ea9acb
jasper zoffmann nilsen <www.jasper@live.dk>
2024-02-06 12:02:56 +0100

    chat configuration file done

---
+++ DomainCoreApi/EFCORE/Configurations/ChatCore/ChatConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using CoreLib.Entities.EchoCore.ChatCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System.Reflection.Metadata;
+
+namespace DomainCoreApi.EFCORE.Configurations.ChatCore
+{
+    public class ChatConfiguration : IEntityTypeConfiguration<Chat>
+    {
+        public void Configure(EntityTypeBuilder<Chat> builder)
+        {
+            builder
+                .HasKey(b => b.Id);
+            builder
+                .Property(b => b.Name)
+                .IsRequired();
+            builder
+                .Property(b => b.TimeCreated).ValueGeneratedOnAdd()
+                .IsRequired();
+            builder.HasMany(b => b.Messages).WithOne(b => b.MessageHolder).HasForeignKey(b => b.MessageHolderId).IsRequired();
+            builder.HasMany(b => b.Mutes).WithOne(b => b.Subject).HasForeignKey(b => b.SubjectId).IsRequired();
+            builder.HasMany(b => b.Invites).WithOne(b => b.Subject).HasForeignKey(b => b.SubjectId).IsRequired();
+            builder.HasMany(b => b.Participants).WithOne(b => b.Subject).HasForeignKey(b => b.SubjectId).IsRequired();
+            builder.HasOne(b => b.Pinboard).WithOne(b => b.Owner).HasForeignKey<ChatPinboard>(b => b.OwnerId).IsRequired();
+        }
+    }
+}
