d4cf6a0d592a4357a2620614e7b46ff4534a4348
jasper zoffmann nilsen <www.jasper@live.dk>
2024-02-08 14:42:13 +0100

    more entity changes

--- CoreLib/Entities/Base/BaseBlock.cs
+++ CoreLib/Entities/Base/BaseBlock.cs
@@ -8,10 +8,10 @@ namespace CoreLib.Entities.Base
 {
     public abstract class BaseBlock<TId, TBlockerEntity, TBlockerEntityId, TBlockedEntity, TBlockedEntityId> : BaseEntity<TId> //done
     {
-        public TBlockerEntityId? BlockerId { get; set; }
-        public TBlockedEntityId? BlockedId { get; set; }
+        public TBlockerEntityId BlockerId { get; set; }
+        public TBlockedEntityId BlockedId { get; set; }
         public DateTime TimeBlocked { get; set; }
-        public TBlockerEntity?  Blocker { get; set; }
-        public TBlockedEntity?  Blocked { get; set; }
+        public TBlockerEntity  Blocker { get; set; }
+        public TBlockedEntity  Blocked { get; set; }
     }
 }
