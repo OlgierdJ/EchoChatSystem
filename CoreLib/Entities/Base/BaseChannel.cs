26464b03cb093355858927eddbb8fb1c3c16746d
jasper zoffmann nilsen <www.jasper@live.dk>
2024-03-18 10:38:35 +0100

    bases should not contain actual types as they are reusable generic implementations of whatever the base represents

--- CoreLib/Entities/Base/BaseChannel.cs
+++ CoreLib/Entities/Base/BaseChannel.cs
@@ -8,16 +8,15 @@ namespace CoreLib.Entities.Base
         public string? Topic { get; set; } //channel topic description
         public int SlowMode { get; set; } //default 0
         public bool IsPrivate { get; set; }
-        public ulong ServerId { get; set; }
-        public Server Server { get; set; }
     }
     public abstract class BaseChannel<TId, TChannelPermission> : BaseChannel<TId>
     {
         public ICollection<TChannelPermission>? Permissions { get; set; }
     }
-    public abstract class BaseChannel<TId, TChannelPermission, TChannelCategory, TChannelCategoryId> : BaseChannel<TId, TChannelPermission>
+    public abstract class BaseChannel<TId, TChannelPermission, TChannelCategory, TChannelCategoryId, TChannelOwner, TChannelOwnerId> : BaseChannel<TId, TChannelPermission>
     {//integrations? webhooks? invites? channelfollows?
-
+        public TChannelOwnerId? OwnerId { get; set; }
+        public TChannelOwner? Owner { get; set; }
         public TChannelCategoryId? CategoryId { get; set; }
         public TChannelCategory? Category { get; set; }
     }
