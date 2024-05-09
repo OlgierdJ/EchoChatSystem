ee232e3027b92b813ddb32d65bf6cf74b18b4c5e
RasmusRBrune <64471940+RasmusRBrune@users.noreply.github.com>
2024-05-09 18:33:22 +0200

    move StringToColor to provider

---
+++ EchoWebapp/EchoWebapp.Client/Provider/StringToColor.cs
@@ -0,0 +1,29 @@
+﻿using MudBlazor;
+
+namespace EchoWebapp.Client.Provider
+{
+    public class StringToColor
+    {
+        public Color ConvertStringToColor(string s)
+        {
+            switch (s)
+            {
+                case "Success":
+                    return Color.Success;
+
+                case "Warning":
+                    return Color.Warning;
+
+                case "Error":
+                    return Color.Error;
+
+                case "Dark":
+                    return Color.Default;
+
+                default:
+                    return Color.Default;
+            }
+            
+        }
+    }
+}
