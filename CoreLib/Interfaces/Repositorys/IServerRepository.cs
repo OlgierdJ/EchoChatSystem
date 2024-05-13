e7d45ddce6185b80ff284a700532b78a2d807038
RasmusRBrune <64471940+RasmusRBrune@users.noreply.github.com>
2024-05-13 12:29:28 +0200

    working on Interface

--- CoreLib/Interfaces/Repositorys/IServerRepository.cs
+++ CoreLib/Interfaces/Repositorys/IServerRepository.cs
@@ -1,10 +1,5 @@
-﻿using CoreLib.Entities.EchoCore.ServerCore;
+﻿using CoreLib.Entities.EchoCore.ServerCore.GeneralCore;
 using CoreLib.Interfaces.Bases;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 
 namespace CoreLib.Interfaces.Repositorys
